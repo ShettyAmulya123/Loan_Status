2bd644023af0eddf2bd5770d717ff3ced0c703b2
ShettyAmulya123 <amulyashetty203@gmail.com>
2026-06-14 22:36:10 +0530

    first commit

---
+++ src/data_exploration.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+df = pd.read_excel("data/loan_approval_dataset.xlsx")
+
+print(df.head())
+print(df.shape)
+print(df.columns)
+print(df.info())
+print(df.describe())
+print(df.isnull().sum())
+
+df.columns = df.columns.str.strip()
+print(df.columns)
+print(df["loan_status"].value_counts())
